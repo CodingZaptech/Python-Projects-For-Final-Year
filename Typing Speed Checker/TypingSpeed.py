d0f64e147b1daa61a5eeb49781b88fba935a3c6d
CodingZap <help@codingzap.com>
2025-09-26 12:46:53 +0530

    Create TypingSpeed.py

---
+++ Typing Speed Checker/TypingSpeed.py
@@ -0,0 +1,11 @@
+# TypingSpeed.py
+# Main runner file for Typing Speed Checker
+
+from TypingSpeedBackend import TypingSpeedApp
+
+def main():
+    app = TypingSpeedApp()
+    app.start_test()
+
+if __name__ == "__main__":
+    main()
