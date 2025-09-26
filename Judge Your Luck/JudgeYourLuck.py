02b3ead463c392de2b68a04763473e729d69426b
CodingZap <help@codingzap.com>
2025-09-26 11:20:38 +0530

    Create JudgeYourLuck.py

---
+++ Judge Your Luck/JudgeYourLuck.py
@@ -0,0 +1,11 @@
+# JudgeYourLuck.py
+# Main runner file for Wheel of Fortune Game
+
+from JudgeYourLuckBackend import WheelOfFortuneApp
+
+def main():
+    app = WheelOfFortuneApp()
+    app.run()
+
+if __name__ == "__main__":
+    main()
