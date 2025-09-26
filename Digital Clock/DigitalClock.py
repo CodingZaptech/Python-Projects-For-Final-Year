79150267156674eb901d6bb3370a12c7f07e27bf
CodingZap <help@codingzap.com>
2025-09-26 11:11:02 +0530

    Create DigitalClock.py

---
+++ Digital Clock/DigitalClock.py
@@ -0,0 +1,11 @@
+# DigitalClock.py
+# Main runner file for Digital Clock project
+
+from DigitalClockBackend import ClockApp
+
+def main():
+    app = ClockApp()
+    app.run()
+
+if __name__ == "__main__":
+    main()
