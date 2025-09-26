e24ef1c0fbb4a55f265758df13ae96428890bc24
CodingZap <help@codingzap.com>
2025-09-26 11:13:14 +0530

    Create input.py

---
+++ Password Generator/input.py
@@ -0,0 +1,9 @@
+# input.py
+# Handles user input for Password Generator
+
+def get_inputs():
+    """
+    Collects password length from user.
+    :return: integer length
+    """
+    return int(input("Enter password length: "))
