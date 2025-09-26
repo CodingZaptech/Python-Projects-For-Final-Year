f8abc4519c9702d770c895de7d675a96f0727283
CodingZap <help@codingzap.com>
2025-09-26 11:09:24 +0530

    Create input.py

---
+++ Countdown Timer/input.py
@@ -0,0 +1,9 @@
+# input.py
+# Handles user input for Countdown Timer
+
+def get_inputs():
+    """
+    Collects time in seconds from user.
+    :return: seconds
+    """
+    return int(input("Enter countdown time in seconds: "))
