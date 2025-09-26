4035bb6108555f5dba4f3793198c00fabc765250
CodingZap <help@codingzap.com>
2025-09-26 11:30:15 +0530

    Create RollOutTheDice.py

---
+++ Roll Out The Dice/RollOutTheDice.py
@@ -0,0 +1,11 @@
+# RollOutTheDice.py
+# Main runner for Dice Game with GUI
+
+from RollOutTheDiceBackend import DiceApp
+
+def main():
+    app = DiceApp()
+    app.run()
+
+if __name__ == "__main__":
+    main()
